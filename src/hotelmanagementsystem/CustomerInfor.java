81c63b4634dc338f2c9bfb38c447fbee17ee671c
Thanh Hương <thanhhuong10102005@gmail.com>
2025-05-06 13:41:41 +0700

    change format Customer Infor

--- src/hotelmanagementsystem/CustomerInfor.java
+++ src/hotelmanagementsystem/CustomerInfor.java
@@ -3,6 +3,7 @@ package hotelmanagementsystem;
 import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,53 +19,67 @@ public class CustomerInfor extends JFrame implements ActionListener {
         this.setLayout(null);
         this.setTitle("Customer Information");
 
-        //Heading label section
-        JLabel jLabel_doc_type = new JLabel("Document Type");
-        jLabel_doc_type.setBounds(60, 10, 100, 20);
-        this.add(jLabel_doc_type);
-
-        JLabel jLabel_cnum = new JLabel("Number");
-        jLabel_cnum.setBounds(200, 10, 100, 20);
+        // Heading label section
+        JLabel jLabel_cnum = new JLabel("CCCD");
+        jLabel_cnum.setBounds(80, 10, 100, 20);
         this.add(jLabel_cnum);
 
-        JLabel jLabel_cname = new JLabel("Name");
-        jLabel_cname.setBounds(300, 10, 100, 20);
+        JLabel jLabel_cname = new JLabel("Họ tên");
+        jLabel_cname.setBounds(200, 10, 100, 20);
         this.add(jLabel_cname);
 
-        JLabel jLabel_cgender = new JLabel("Gender");
-        jLabel_cgender.setBounds(420, 10, 100, 20);
+        JLabel jLabel_cgender = new JLabel("Giới tính");
+        jLabel_cgender.setBounds(300, 10, 100, 20);
         this.add(jLabel_cgender);
 
-        JLabel jLabel_ccountry = new JLabel("Country");
-        jLabel_ccountry.setBounds(520, 10, 100, 20);
+        JLabel jLabel_ccountry = new JLabel("Quốc tịch");
+        jLabel_ccountry.setBounds(420, 10, 100, 20);
         this.add(jLabel_ccountry);
 
-        JLabel jLabel_croom = new JLabel("Room Number");
-        jLabel_croom.setBounds(620, 10, 100, 20);
+        JLabel jLabel_cphone = new JLabel("Số điện thoại");
+        jLabel_cphone.setBounds(520, 10, 100, 20);
+        this.add(jLabel_cphone);
+
+        JLabel jLabel_croom = new JLabel("Số phòng");
+        jLabel_croom.setBounds(630, 10, 100, 20);
         this.add(jLabel_croom);
 
-        JLabel jLabel_ccheckin = new JLabel("Checkin Time");
+        JLabel jLabel_ccheckin = new JLabel("Checkin");
         jLabel_ccheckin.setBounds(750, 10, 100, 20);
         this.add(jLabel_ccheckin);
 
-        JLabel jLabel_cdeposit = new JLabel("Deposit");
-        jLabel_cdeposit.setBounds(870, 10, 100, 20);
+        JLabel jLabel_cdeposit = new JLabel("Tiền gửi trước");
+        jLabel_cdeposit.setBounds(850, 10, 100, 20);
         this.add(jLabel_cdeposit);
 
-        //Table section
+        // Table section
         jTable = new JTable();
-        jTable.setBounds(50, 40, 900, 400);
+        jTable.setBounds(40, 40, 900, 400);
         this.add(jTable);
 
         try {
             Connect c = new Connect();
-            ResultSet rs = c.s.executeQuery("select * from customer");
+            ResultSet rs = c.s.executeQuery("select KH.CCCD, KH.HoTen, KH.GioiTinh, KH.QuocTich, KH.SDT_KH, " +
+                    "P.SoPhong, HD.NgayNhan, KH.DuaTruoc from HoaDon HD join KhachHang KH on KH.CCCD=HD.CCCD " +
+                    "join Phong P on P.SoPhong= HD.SoPhong");
             jTable.setModel(DbUtils.resultSetToTableModel(rs));
+            jTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+            jTable.setRowHeight(30);
+            jTable.setShowVerticalLines(false); // Ẩn đường kẻ dọc
+            DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+            centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+            jTable.setGridColor(Color.GRAY); // Đặt màu cho đường kẻ
+
+
+            for (int i = 0; i < jTable.getColumnCount(); i++) {
+                jTable.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+            }
+
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            JOptionPane.showMessageDialog(this, "Error: " + e.getMessage());
         }
 
-        //back button section
+        // Back button section
         back = new JButton("BACK");
         back.setBounds(420, 500, 120, 30);
         back.setBackground(Color.BLACK);
@@ -77,6 +92,7 @@ public class CustomerInfor extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        this.setResizable(false);
     }
 
     public static void main(String[] args) {
